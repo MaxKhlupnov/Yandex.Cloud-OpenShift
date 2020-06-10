e0d31fdca350a25df999464ed4f5fef674bc84d1
khlupnov <m.khlupnov@gmail.com>
2020-06-10 21:21:12 +0300

    close

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -1,15 +1,46 @@
 # Load Balancing For HA Applications via Yandex Load Balancer
 # see https://github.com/redhat-cop/openshift-playbooks/blob/master/playbooks/installation/load_balancing.adoc
-resource "yandex_lb_target_group" "okd_master_target" {
- count       = var.okd_kube_master_num
-  name      = "lbgrp-mgmt-8443-${count.index}"
-  region_id = element(var.okd_availability_zones, count.index)
 
+locals {
+  //masters = [yandex_compute_instance.master.*.id, yandex_compute_instance.master.*.network_interface.0.ip_address]
+  subnet_mapping = [for i, master in yandex_compute_instance.master : { master_ip : master.network_interface.0.ip_address, subnet_id : master.network_interface.0 }]
+}
+
+ //  Use `subnet_mapping` to attach EIPs
+  
+/*resource "yandex_lb_target_group" "okd_master_target" {
+  name      = "lbgrp-mgmt-8443"
+  for_each = {
+    for i, master in yandex_compute_instance.master: 
+            { 
+              ip_address : master.network_interface.0.ip_address, 
+              subnet_id : master.network_interface.subnet_id
+            } => i
+  }
+}*/
+
+resource "yandex_lb_target_group" "okd_master_target" {
+  name      = "lb-grp-mgmt-8443"
+  for_each = {
+    for ic in range(var.okd_kube_master_num): "${ic}" => ic
+  }
   target {
-    subnet_id = element(yandex_vpc_subnet.subnet, count.index).id
-    address   = element(yandex_compute_instance.master,count.index).network_interface.0.ip_address
+    subnet_id = element(yandex_compute_instance.master, each.key).network_interface.0.subnet_id
+    address   = element(yandex_compute_instance.master,each.key).network_interface.0.ip_address
   }
 }
+
+
+
+
+
+ /* for_each = { for m in local.subnet_mapping: m.master_ip => m } 
+     
+  depends_on = [
+    yandex_compute_instance.master
+  ]
+  */
+
 /*
 resource "yandex_lb_network_load_balancer" "foo" {
   name = "my-network-load-balancer"
