0feca1d2bb49a1533728beba57eba8774c74747d
khlupnov <m.khlupnov@gmail.com>
2020-06-11 00:05:39 +0300

    Target groups creation

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -1,15 +1,24 @@
 # Load Balancing For HA Applications via Yandex Load Balancer
 # see https://github.com/redhat-cop/openshift-playbooks/blob/master/playbooks/installation/load_balancing.adoc
 
-resource "yandex_lb_target_group" "okd_master_target" {
-  count       = var.okd_kube_master_num
-  name      = "lb-grp-mgmt-${count.index}"  
-
-  target {
-    subnet_id = element(yandex_compute_instance.master, count.index).network_interface.0.subnet_id
-    address   = element(yandex_compute_instance.master,count.index).network_interface.0.ip_address
-  }
-
+locals {
+  master_mapping = flatten([for i, master in yandex_compute_instance.master : 
+          {
+               ip_address = master.network_interface.0.ip_address, 
+               subnet_id = master.network_interface.0.subnet_id 
+          }])
 }
 
-
+resource "yandex_lb_target_group" "okd_master_target" {
+ name      = "lb-grp-mgmt-8443"
+ dynamic "target" {
+    for_each = local.master_mapping
+    content{
+      address = target.value.ip_address
+      subnet_id = target.value.subnet_id 
+  
+    }
+ }
+}
+ 
+  
